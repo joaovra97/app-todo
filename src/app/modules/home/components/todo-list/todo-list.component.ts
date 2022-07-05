c61bd0e72833eb7929038f3243fea246fa16adc4
João Victor Rodrigues Alves <joao.rodrigues@kewin.com>
2022-07-05 10:42:50 -0300

    validações to-do list

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { first } from 'rxjs';
 
 //Interfaces
 import { TaskList } from '../../model/task-list';
@@ -8,13 +9,14 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(last.checked) - Number(first.checked));
   }
 
   public deleteItemTaskList(event: number){
@@ -32,4 +34,12 @@ export class TodoListComponent implements OnInit {
     this.taskList.push({task: event, checked: false});
   }
 
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+
+      if(confirm)
+        this.deleteItemTaskList(index);
+    }
+  }
 }
